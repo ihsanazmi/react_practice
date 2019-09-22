3a3a8439df236653850909dcd4e1f76ab06d63cc
ihsanazmi <ihsanazmiii@gmail.com>
2019-09-23 00:27:35 +0700

    adding folder components, file App.jsx and index.js

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Component App</h1>
+            </div>
+        )
+    }
+}
+
+//Agar bisa di gunakan di file lain, maka kita harus export
+export default App
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+// App adalah komponen induk dari semua komponen yang ada
+// Komponen yang dibutuhkan akan berkumpul di App.js
+// Di import dari alamat file App berada, ini hanya memungkinkan jika file App sudah melakukan proses export
+import App from './components/App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+)
