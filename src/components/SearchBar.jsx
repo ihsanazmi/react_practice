01208d31b2b20e120aad5e6d7fa396c858a3e425
ihsanazmi <ihsanazmiii@gmail.com>
2019-09-23 10:32:31 +0700

    adding preventdefault

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
 
 class SearchBar extends Component {
+
+    state = {
+        term: ''
+    }
+
     render() {
         return (
             <div>
-                <form className="form-group">
-                    <input type="text" className="form-control"/>
+                <form onSubmit={(event)=>{event.preventDefault()}} className="form-group">
+                    <input 
+                        type="text" 
+                        className="form-control"
+                        onChange={(event)=>{console.log(event.target.value)}}
+                    />
                 </form>
             </div>
         )
@@ -13,3 +22,8 @@ class SearchBar extends Component {
 }
 
 export default SearchBar
+
+// Event handler, bertugas men-handle ketika suatu event terjadi
+    //event: onClick, onSubmit, onChange
+// onChange memungkinkan kita untuk memanggil function ketika ada perubahan di text input
+// onSubmit memungkinkan kita untuk memanggil function ketika kita tap 'enter'
